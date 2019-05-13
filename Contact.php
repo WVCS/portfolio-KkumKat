5900ffa41cd7b6a65925b9a177939429ec0564c5
KkumKat <49921417+KkumKat@users.noreply.github.com>
2019-05-13 08:16:51 -0700

    Update Contact.php

--- Contact.php
+++ Contact.php
@@ -1,13 +1,14 @@
-<?php
-$name = $_POST['name'];
-$email = $_POST['email'];
-$phone = $_POST['telephone'];
-$subject = $_POST['subject'];
-$message = $_POST['message'];
-$formcontent="From: $name $email $phone \n Message: $message";
-$recipient = "kassidypeep@gmail.com";
-$emailSubject = "$subject";
-$mailheader = "From: $email \r\n";
-mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
-echo "Your email has been sent. I'll try to get back to you as soon as possible. Thank You!" . " -" . "<a href='index.html' style='text-decoration:none;color:#ff0099;'> Return to the home page</a>";
-?>
+var $form = $('form#contact_form'),
+    url = 'https://script.google.com/macros/s/AKfycbzAFW8OpqzlxC-AXuRVA6l7BkgUI-1zIWXNDHgGqRd7YscdkVw5/exec'
+
+$('#form_button').on('click', function(e) {
+  e.preventDefault();
+  var jqxhr = $.ajax({
+    url: url,
+    method: "GET",
+    dataType: "json",
+    data: $form.serializeObject()
+  }).success(
+    // do something
+  );
+})
