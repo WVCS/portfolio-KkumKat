da7a4a26b8b3e90015705a013d91a170218cb0a4
KkumKat <49921417+KkumKat@users.noreply.github.com>
2019-06-12 09:07:18 -0700

    Add files via upload

---
+++ contactthanks.php
@@ -0,0 +1,25 @@
+<!DOCTYPE HTML>
+
+<head>
+	<title>Kassidy Baier - Contact Me - Success</title>
+	
+	<link rel="stylesheet" type="text/css" href="style.css" />
+</head>
+
+<body>
+
+    <?php include("../header.php"); ?>
+
+	<div id="page-wrap">
+			
+		<h1>Your message has been sent!</h1><br />
+		
+		<p><a href="Contact Me.html">Back to Contact Form</a></p>
+	
+	</div>
+	
+	<?php include("../footer.php"); ?>
+
+</body>
+
+</html>
